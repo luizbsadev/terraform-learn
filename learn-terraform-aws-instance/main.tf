3c78219b9f0cbd3406e38713f7cf3c250e8bf743
luizbsadev <luiz.bsa.olv@gmail.com>
2023-10-18 09:28:05 -0300

    automatizando a criação da pasta e lançando no servidor com o terraform

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -17,8 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-03f65b8614a860c29"
   instance_type = "t2.micro"
   key_name      = "IaC-alura"
+  user_data     = <<-EOF
+                  #!/bin/bash
+                  cd /home/ubuntu
+                  echo "<h1> feito com terraform <h1>" > index.html
+                  nohup busybox httpd -f -p 8080 &  
+                  EOF
 
   tags = {
-    Name = "primeira instancia"
+    Name = "teste automatizando"
   }
 }
